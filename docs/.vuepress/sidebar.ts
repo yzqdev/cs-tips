c8fce828d5594317279a51089d1838025d6de1e6
yzqdev <yzqdev@outlook.com>
2022-09-19 14:45:08 +0800

    add webtypes

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -2,6 +2,7 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
   "/frontend/": "structure",
+  "/tool/": "structure",
 
   "/java-tip/": "structure",
   "/go-tip/": "structure",
